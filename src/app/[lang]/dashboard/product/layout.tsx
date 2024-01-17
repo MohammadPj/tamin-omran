5376ee19473e8951d7466eb17cb78b540bea7903
Muhammad <mohammadpoorjamal@gmail.com>
2024-01-17 11:39:28 +0330

    remove engine tab from product

--- src/app/[lang]/dashboard/product/layout.tsx
+++ src/app/[lang]/dashboard/product/layout.tsx
@@ -1,22 +1,21 @@
-import React, {FC} from 'react';
-import {Box, Stack} from "@mui/material";
-import CustomTab, {ITab} from "~/components/custom-mui/custom-tab/CustomTab";
+import React, { FC } from "react";
+import { Box, Stack } from "@mui/material";
+import CustomTab, { ITab } from "~/components/custom-mui/custom-tab/CustomTab";
 
 interface BrochureLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
-const BrochureLayout: FC<BrochureLayoutProps>  = ({children}) => {
+const BrochureLayout: FC<BrochureLayoutProps> = ({ children }) => {
   const tabs: ITab[] = [
-    {href: "/dashboard/product", label: "محصول"},
-    {href: "/dashboard/product/category", label: "دسته بندی"},
-    {href: "/dashboard/product/brand", label: "برند"},
-    {href: "/dashboard/product/engine", label: "موتور"},
-  ]
+    { href: "/dashboard/product", label: "محصول" },
+    { href: "/dashboard/product/category", label: "دسته بندی" },
+    { href: "/dashboard/product/brand", label: "برند" },
+    // {href: "/dashboard/product/engine", label: "موتور"},
+  ];
 
   return (
-    <Stack height={'100%'}>
-      <CustomTab tabs={tabs} tabsProps={{sx: {mb: 4}}} />
-
+    <Stack height={"100%"}>
+      <CustomTab tabs={tabs} tabsProps={{ sx: { mb: 4 } }} />
       {children}
     </Stack>
   );
